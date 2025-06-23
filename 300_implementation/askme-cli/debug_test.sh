f68ec51cb8c43322979720abc3de3a71c8565d7a
vn6295337 <vn6295337@gmail.com>
2025-06-23 07:57:33 +0530

    Cleaned up folders

--- 300_implementation/askme-cli/debug_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Testing CLI with debug..."
-echo "What is 2+2?" > /tmp/test.txt
-timeout 30 ./gradlew cliApp:run --args='-f /tmp/test.txt -m mistral' --quiet
--- 300_implementation/askme-cli/src/commonMain/kotlin/com/askme/api/AiProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.askme.api
-
-import com.askme.model.Answer
-
-interface AiProvider {
-    val name: String
-    suspend fun askQuestion(question: String): Answer
-}
